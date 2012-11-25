3a25b22ada95da252343401a7eb732eddbb2200c
unknown <tafeng.dxx@JHGH-RMPPZY3OCD.taobao.ali.com>
2012-11-25 19:32:28 +0800

    add comment

--- regression/task_manager.js
+++ regression/task_manager.js
@@ -54,7 +54,9 @@ exports.TaskManager = {
         this._bindEvents();
         this.fetchTask();
     },
-    /*用例结束，插入数据库结果*/
+    /*用例结束，插入数据库结果
+    * 每满50个才进行一次性的插入，防止频繁的操作数据库
+    */
     report:function (task, info) {
         reportBuffer.push({task:task, info:info});
         //缓冲区未满，且任务队列未空
@@ -62,23 +64,14 @@ exports.TaskManager = {
             return;
         }
 
-        //  var content = qs.stringify(reportBuffer);
-        var options = {
-            host:API['report'],
-            port:80,
-            path:'/report',
-            method:'POST',
-            headers:{
-                'Content-Type':'application/x-www-form-urlencoded',
-                'Content-Length':content.length
-            }
-        };
         http.post(API['report'], reportBuffer,function (res) {
             console.log('STATUS: ' + res.statusCode);
             console.log('HEADERS: ' + JSON.stringify(res.headers));
         }).on('error', function (e) {
                 console.log('problem with report: ' + e.message);
-            });
+        });
+
+        //清除报告缓冲区
         reportBuffer = [];
         //任务队列空，则取数据
         !taskCache.total && this.fetchTask();
@@ -113,9 +106,11 @@ exports.TaskManager = {
         /*更新浏览器用例队列*/
         'browserTypes':function (types) {
             var i, len = types.length, tasks = taskCache.tasks;
+            //对每种浏览器复制一份用例引用
             for (i = len - 1; i > -1; --i) {
                 this.browserCache[types[i]] = taskCache.tasks.slice(0);
             }
+            //对每个用例，将计数器至为浏览器类型数量
             for (i = taskCache.length - 1; i > -1; --i) {
                 tasks[i].browserCount = tasks[i].total_specs = len;
             }
@@ -131,13 +126,15 @@ exports.TaskManager = {
 
         /*用例测完*/
         'taskFinish':function (task, info) {
-            //所有浏览器都已测试完毕，发送报告
+            //用例测完，减少计数器
             if (--task.browserCount == 0) {
+                //计数器为0，减少用例池的总数，同时报告该用例结果
                 --taskCache.total;
                 this.report(task, info);
             }
         }
     },
+    //事件绑定
     _bindEvents:function () {
         var evts = this._eventsMap;
         for (var k in evts) {
@@ -161,6 +158,9 @@ exports.BrowserManager = {
     _eventsMap:{
         'dataUpdated':function () {
             //fires 'browserTypes' event
+        },
+        'task': function (task) {
+
         }
     },
     _socketEventsMap:{
