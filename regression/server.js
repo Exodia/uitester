7bda56d26ce9191647603376a61bd4da0ccda68b
unknown <tafeng.dxx@JHGH-RMPPZY3OCD.taobao.ali.com>
2012-12-08 20:50:41 +0800

    add file

---
+++ regression/server.js
@@ -0,0 +1,62 @@
+ /**
+ * UITester Server Module
+ * @author: LongGang <tblonggang@gmail.com>
+ * require:
+ *      1. underscore (npm install unerscore)
+ *      2. socket.IO (npm install socket.io)
+ */
+var io = require('socket.io').listen(8080, {'log level': 0});
+    userAgent = require('user-agent');
+
+var ClientManager = require('client-mgr'),
+    TaskManager = require('task-mgr').TaskManager,
+    EventManager = require('event-mgr').EventManager;
+
+ClientManager.init();
+TaskManager.init();
+
+ var guid = -1;
+
+ function Client(socket) {
+     this.socket = socket;
+     this.id = ++guid;
+     this.task = this.clientType = this.reportData = this.userAgent = null;
+ }
+
+ Client.prototype.runTask = function(task) {
+     this.socket.emit('console:task', task);
+     this.task = task;
+     EventManager.emit('client:busy', this.id);
+ }
+
+
+io.sockets.on('connection', function (socket) {
+
+    // wrapper object
+    var clientObject = new Client(socket);
+
+    socket.on('disconnect', function (){
+        EventManager.emit('client:disconnect', clientObject);
+    });
+
+    // Register client after Socket.IO connected
+    socket.on('console:register', function (data){
+        //1、设置浏览器类型
+        clientObject.userAgent = userAgent.parse(data.userAgent);
+        //2、触发注册事件
+        EventManager.emit('client:register', clientObject);
+    });
+
+    // Client task finished, report send back
+    socket.on('console:task_finish', function (data){
+        console.log('console:task_finish', clientObject.id);
+        //1、置入报告数据
+        clientObject.task.reportData = data.reportData;
+        //2、触发任务结束事件
+        EventManager.emit('client:task_finish', clientObject.task);
+        //3、撤销任务
+        clientObject.task = null;
+        //4、触发浏览器空闲事件
+        EventManager.emit('client:available', clientObject);
+    });
+});
